d3c6b386a33173c33e4e3a878216565c5d93d7e0
ru76dig <niklas.thiem@campus.lmu.de>
2019-05-22 19:33:49 +0200

    added helper with useful methods

---
+++ Grand Theft Candy/Grand Theft Candy/Helpers.swift
@@ -0,0 +1,27 @@
+//
+//  Helpers.swift
+//  Grand Theft Candy
+//
+//  Created by Niklas on 22.05.19.
+//  Copyright © 2019 Gruppe02. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import SpriteKit
+
+// Collection of useful methods which have a good chance to be reused
+
+    // Gets the root node for a given scn (useful for imported models)
+    public func getRootNodeOfScn (scnname: String) -> SCNNode
+    {
+        let scene = SCNScene(named: scnname)!
+        return scene.rootNode.childNodes[0]
+    }
+    
+    // Calculates the radian for a given degree (Scenekit use rads for rotation)
+    public func degreeToRad (degree: Float) -> Float
+    {
+        return Float(Double.pi/180) * degree
+    }
+
