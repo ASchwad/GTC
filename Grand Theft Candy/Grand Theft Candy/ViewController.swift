e4275bc845a16dabef59aa076f20eecbb40e01d8
ru76dig <niklas.thiem@campus.lmu.de>
2019-05-22 14:36:25 +0200

    reinitialized project

---
+++ Grand Theft Candy/Grand Theft Candy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grand Theft Candy
+//
+//  Created by admin on 22.05.19.
+//  Copyright © 2019 Gruppe02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
